91e11a0f1230392a9c13033483dd776a8f0fe411
tech-striker <ravikantgautamjazz@gmail.com>
2021-08-20 12:26:27 +0530

    Chapter 1

---
+++ README.md
@@ -0,0 +1,6 @@
+#Chapter-1
+
+##Points to remember
+1. hello function is use to basic return
+2. get_component function is use when we need path parameter. for example, /componenet/1/
+3. get_read function is used when we need query parameter
--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get('/')
 async def hello():
     return {"context": "Hello World"}
+
+@app.get('/component/{id}') # path parameter 
+async def get_componenet(id: int):
+    return {"id": id}
+
+@app.get('/component/') # query parameter /component/?id=2&text=Hello world
+async def get_read(id: int, text: str):
+    return {"id": id, "text": text}
