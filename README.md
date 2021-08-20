9dfd78ec300ace15de84368a813405afc4e8a29b
tech-striker <ravikantgautamjazz@gmail.com>
2021-08-20 13:29:20 +0530

    Chapter-2

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-#Chapter-1
+#Chapter-2
 
 ##Points to remember
-1. hello function is use to basic return
-2. get_component function is use when we need path parameter. for example, /componenet/1/
-3. get_read function is used when we need query parameter
+1. User of BaseModel class to strict datatype and request body
+2. We can use different class models for getting and sending the response body
+3. use "response_model_exclude" and "response_model_include" to include or exclude specific key from the response body
--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+
+class Package(BaseModel):
+    name: str
+    number: int
+    description: Optional[str] = None
+
+class PackageIN(BaseModel):
+    secret_id: str
+    name: str
+    number: int
+    description: Optional[str] = None
+
 
 app = FastAPI()
 
@@ -13,3 +27,21 @@ async def get_componenet(id: int):
 @app.get('/component/') # query parameter /component/?id=2&text=Hello world
 async def get_read(id: int, text: str):
     return {"id": id, "text": text}
+
+@app.post("/package/{my_id}") # body response data oon POST request
+async def make_package(my_id: int,pack: Package):
+    return {"id": my_id, **pack.dict()}
+
+
+@app.post("/packagesecret/{my_id}", response_model=Package) # This is to hide secret data/variable
+async def make_package_secret(my_id: int,pack: PackageIN):
+    return {"id": my_id, **pack.dict()}
+
+
+@app.post("/packagesecretexclude/{my_id}", response_model=Package, response_model_exclude=('description',)) # This is to remove the optional fields
+async def make_package_secret_exclude(my_id: int,pack: PackageIN):
+    return {"id": my_id, **pack.dict()}
+
+@app.post("/packagesecretinclude/{my_id}", response_model=Package, response_model_include=('description',)) # This is to include only the specific fields
+async def make_package_secret_include(my_id: int,pack: PackageIN):
+    return {"id": my_id, **pack.dict()}
